e1eb3298598c83506e2f14494659789289dc97f5
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-08-15 16:51:59 +0300

    fix: .map.map

--- src/features/posts/PostList.js
+++ src/features/posts/PostList.js
@@ -9,7 +9,7 @@ const PostList = () => {
 
     const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
 
-    const renderedPosts = orderedPosts.map.map(post => (
+    const renderedPosts = orderedPosts.map(post => (
         <article key={posts.id} className='flex flex-col items-start gap-2 py-7 px-3 border border-zinc-400 w-96 rounded-md'>
             <h3 className='text-xl font-medium tracking-wider'>{post.title}</h3>
             <p>{post.content.substring(0,100)}</p>
