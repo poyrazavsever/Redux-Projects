d95051aaabf0e29b147b2b27c7ea21641a45cea0
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-08-15 16:51:48 +0300

    feat: ordered post done

--- src/features/posts/PostList.js
+++ src/features/posts/PostList.js
@@ -9,14 +9,14 @@ const PostList = () => {
 
     const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
 
-    const renderedPosts = orderedPosts.map(post => (
+    const renderedPosts = orderedPosts.map.map(post => (
         <article key={posts.id} className='flex flex-col items-start gap-2 py-7 px-3 border border-zinc-400 w-96 rounded-md'>
             <h3 className='text-xl font-medium tracking-wider'>{post.title}</h3>
             <p>{post.content.substring(0,100)}</p>
             <div className='flex justify-between items-center w-full mt-2'>
                 <span className='text-sm font-semibold tracking-wider'>
                     <PostAuthor userId={post.userId}/>
-               </span>
+                </span>
                 <span className='text-sm font-semibold tracking-wider'>
                     <TimeAgo timestamp={post.date}/>
                 </span>
