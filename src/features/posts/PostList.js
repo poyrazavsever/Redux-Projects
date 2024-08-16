c11f3ee56dc235253b07289561e4cb1baa162f54
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-08-16 03:13:41 +0300

    feat: done middleware

--- src/features/posts/PostList.js
+++ src/features/posts/PostList.js
@@ -1,9 +1,7 @@
 import React, {useEffect} from 'react'
 import { selectAllPost, getPostError, getPostsStatus, fetchPosts } from './postsSlice'
 import { useSelector, useDispatch} from 'react-redux'
-import PostAuthor from './PostAuthor'
-import TimeAgo from './TimeAgo'
-import ReactionButtons from './ReactionButton'
+import PostsExcerpt from './PostsExcerpt'
 
 const PostList = () => {
 
@@ -20,31 +18,22 @@ const PostList = () => {
         }
     }, [postStatus, dispatch])
 
-    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+    let content;
+    if (postStatus === 'loading') {
+        content = <p>"Loading..."</p>;
+    } else if (postStatus === 'succeeded') {
+        const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+        content = orderedPosts.map(post => <PostsExcerpt key={post.id} post={post} />)
+    } else if (postStatus === 'failed') {
+        content = <p>{error}</p>;
+    }
 
-    const renderedPosts = orderedPosts.map(post => (
-        <article key={posts.id} className='flex flex-col items-start gap-2 py-7 px-3 border border-zinc-400 w-96 rounded-md'>
-            <h3 className='text-xl font-medium tracking-wider'>{post.title}</h3>
-            <p>{post.content.substring(0,100)}</p>
-            <div className='flex justify-between items-center w-full mt-2'>
-                <span className='text-sm font-semibold tracking-wider'>
-                    <PostAuthor userId={post.userId}/>
-                </span>
-                <span className='text-sm font-semibold tracking-wider'>
-                    <TimeAgo timestamp={post.date}/>
-                </span>
-            </div>
-            <span className='text-sm font-semibold tracking-wider'>
-                <ReactionButtons post={post}/>
-            </span>
-        </article>
-    ))
 
     return (
         <section className='flex flex-col items-center justify-center mt-16'>
             <h2 className='text-6xl font-semibold tracking-wider underline'>Posts</h2>
             <div className='mt-8 flex flex-col items-start gap-4'>
-                {renderedPosts}
+                {content}
             </div>
         </section>
     )
---
+++ src/features/posts/PostsExcerpt.js
@@ -0,0 +1,25 @@
+import PostAuthor from './PostAuthor'
+import TimeAgo from './TimeAgo'
+import ReactionButtons from './ReactionButton'
+
+const PostsExcerpt = ({ post }) => {
+  return (
+        <article className='flex flex-col items-start gap-2 py-7 px-3 border border-zinc-400 w-96 rounded-md'>
+            <h3 className='text-xl font-medium tracking-wider'>{post.title}</h3>
+            <p>{post.body.substring(0,100)}</p>
+            <div className='flex justify-between items-center w-full mt-2'>
+                <span className='text-sm font-semibold tracking-wider'>
+                    <PostAuthor userId={post.userId}/>
+                </span>
+                <span className='text-sm font-semibold tracking-wider'>
+                    <TimeAgo timestamp={post.date}/>
+                </span>
+            </div>
+            <span className='text-sm font-semibold tracking-wider'>
+                <ReactionButtons post={post}/>
+            </span>
+        </article>
+  )
+}
+
+export default PostsExcerpt
