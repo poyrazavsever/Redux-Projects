b2864607e59ee09dd5481d25c71ab42ad41866c8
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-08-16 13:59:55 +0300

    feat: done add post function

--- src/features/posts/AddPostForm.js
+++ src/features/posts/AddPostForm.js
@@ -1,6 +1,7 @@
 import {useState} from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 
+import { addNewPost } from './postsSlice';
 import {postAdded} from "./postsSlice"
 import { selectAllUsers } from '../users/usersSlice';
 
@@ -11,6 +12,7 @@ const AddPostForm = () => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('')
   const [userId, setUserId] = useState('')
+  const [addRequestStatus, setAddRequestStatus] = useState('idle')
 
   const users = useSelector(selectAllUsers);
 
@@ -18,20 +20,25 @@ const AddPostForm = () => {
   const onContentChanged = e => setContent(e.target.value)
   const onAuthorChanged = e => setUserId(e.target.value)
 
+  
+  const canSave = [title, content, userId].every(Boolean) && addRequestStatus === 'idle';
+
   const onSavePostClick = () => {
-    if (title && content) {
-      dispatch(
-        postAdded(title, content, userId)
-      )
+      if (canSave) {
+        try {
+            setAddRequestStatus('pending')
+            dispatch(addNewPost({ title, body: content, userId })).unwrap()
 
-      setTitle('')
-      setContent('')
-      setUserId('')
-
+            setTitle('')
+            setContent('')
+            setUserId('')
+        } catch (err) {
+            console.error('Failed to save the post', err)
+        } finally {
+            setAddRequestStatus('idle')
+        }
     }
   }
-
-  const canSave = Boolean(title) && Boolean(content) && Boolean(userId)
 
   const usersOptions = users.map((user) => (
     <option key={user.id} value={user.id}>
